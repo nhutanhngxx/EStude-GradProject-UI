758b407b2adecbf3f7e6c2760280b0d3d5b56e60
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-08-31 15:41:42 +0700

    Add schoolService for managing school data operations

---
+++ EStude-Web-UI/src/services/schoolService.js
@@ -0,0 +1,50 @@
+import config from "../config/config.js";
+
+const endpoints = {
+  addSchool: "/api/schools",
+  getSchool: "/api/schools",
+  //   getAllSchools: "/api/schools",
+  //   updateSchool: "/api/schools",
+  //   deleteSchool: "/api/schools",
+};
+
+const schoolService = {
+  addSchool: async (school) => {
+    try {
+      const response = await fetch(`${config.BASE_URL}${endpoints.addSchool}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(school),
+      });
+      if (!response.ok) {
+        throw new Error("Thêm trường thất bại");
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Lỗi khi thêm trường:", error);
+      return null;
+    }
+  },
+  getSchool: async (schoolId) => {
+    try {
+      const response = await fetch(
+        `${config.BASE_URL}${endpoints.getSchool}/${schoolId}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Lấy thông tin trường thất bại");
+      }
+      const result = await response.json();
+      return result; // { success, message, ... }
+    } catch (error) {
+      console.error("Lỗi khi lấy thông tin trường:", error);
+      return null;
+    }
+  },
+};
+
+export default schoolService;
