f52cd17dd8005760b3db1068d5f560e01164fc38
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-09-01 20:22:48 +0700

    Add classSubjectService for managing class subjects API interactions

---
+++ EStude-Web-UI/src/services/classSubjectService.js
@@ -0,0 +1,78 @@
+import config from "../config/config.js";
+
+const endpoints = {
+  addClassSubject: "/api/class-subjects",
+  getTeacherClassSubjects: `/api/teachers/classes/{teacherId}/class-subjects`,
+  getAllClassSubjects: "/api/class-subjects",
+};
+
+const classSubjectService = {
+  addClassSubject: async (classSubject) => {
+    try {
+      const response = await fetch(
+        `${config.BASE_URL}${endpoints.addClassSubject}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(classSubject),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Thêm môn học cho lớp thất bại");
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Lỗi khi thêm môn học cho lớp:", error);
+      return null;
+    }
+  },
+  getTeacherClassSubjects: async (teacherId) => {
+    try {
+      const response = await fetch(
+        `${config.BASE_URL}${endpoints.getTeacherClassSubjects.replace(
+          "{teacherId}",
+          teacherId
+        )}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (!response.ok) {
+        throw new Error(
+          "Lấy danh sách lớp học giảng dạy của giáo viên thất bại"
+        );
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error(
+        "Lỗi khi lấy danh sách lớp học giảng dạy của giáo viên:",
+        error
+      );
+      return null;
+    }
+  },
+  getAllClassSubjects: async () => {
+    try {
+      const response = await fetch(
+        `${config.BASE_URL}${endpoints.getAllClassSubjects}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Lấy danh sách môn học của lớp thất bại");
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Lỗi khi lấy danh sách môn học của lớp:", error);
+      return null;
+    }
+  },
+};
+
+export default classSubjectService;
