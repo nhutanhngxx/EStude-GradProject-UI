bb635c8cd27c2659849fc53caac635775437c785
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-09-01 20:22:42 +0700

    Add classService for managing class-related API interactions

---
+++ EStude-Web-UI/src/services/classService.js
@@ -0,0 +1,93 @@
+import config from "../config/config.js";
+
+const endpoints = {
+  addClass: "/api/classes",
+  getClassById: "/api/classes/{classId}",
+  getAllClasses: "/api/classes",
+  getClassesBySchoolId: `/api/classes/school/{schoolId}`,
+};
+
+const classService = {
+  addClass: async (classData) => {
+    try {
+      const response = await fetch(`${config.BASE_URL}${endpoints.addClass}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(classData),
+      });
+      if (!response.ok) {
+        throw new Error("Thêm lớp thất bại");
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Lỗi khi thêm lớp:", error);
+      return null;
+    }
+  },
+  getAllClasses: async () => {
+    try {
+      const response = await fetch(
+        `${config.BASE_URL}${endpoints.getAllClasses}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Lấy danh sách lớp thất bại");
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Lỗi khi lấy danh sách lớp:", error);
+      return null;
+    }
+  },
+  getClassById: async (classId) => {
+    try {
+      const response = await fetch(
+        `${config.BASE_URL}${endpoints.getClassById.replace(
+          "{classId}",
+          classId
+        )}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Lấy thông tin lớp thất bại");
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Lỗi khi lấy thông tin lớp:", error);
+      return null;
+    }
+  },
+  getClassesBySchoolId: async (schoolId) => {
+    try {
+      const response = await fetch(
+        `${config.BASE_URL}${endpoints.getClassesBySchoolId.replace(
+          "{schoolId}",
+          schoolId
+        )}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Lấy danh sách lớp thất bại");
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Lỗi khi lấy danh sách lớp:", error);
+      return null;
+    }
+  },
+};
+
+export default classService;
