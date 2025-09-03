076f9e30fb0deafa91156ed9c52458757e10984f
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-09-04 00:12:09 +0700

    feat: Implement authService for user authentication with login and logout functionality

---
+++ EStude-App-UI/src/services/authService.js
@@ -0,0 +1,42 @@
+import config from "../configs/config";
+
+const endpoints = {
+  loginStudent: "/api/auth/login-student",
+  logout: "/api/auth/logout",
+};
+
+const authService = {
+  login: async ({ username, password }) => {
+    try {
+      const res = await fetch(`${config.BASE_URL}${endpoints.loginStudent}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+      const json = await res.json();
+      if (json.success) {
+        return { user: json.data, token: json.token };
+      }
+      return null;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  },
+  logout: async (token) => {
+    try {
+      const res = await fetch(`${config.BASE_URL}${endpoints.logout}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return res.ok;
+    } catch (err) {
+      console.error("Logout error:", err);
+      return false;
+    }
+  },
+};
+
+export default authService;
