0f5d7d72024c50dfafe33f11f515a2db84713a81
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-09-04 00:13:45 +0700

    feat: Remove apiClient.js as part of service refactoring

--- EStude-App-UI/src/services/apiClient.js
+++
@@ -1,6 +0,0 @@
-// Xử lý việc cấu hình và tạo một instance của Axios
-// để thực hiện các yêu cầu HTTP(GET, POST, PUT, DELETE, PATCH)
-// trong một ứng dụng React Native sử dụng Expo.
-// Nó cũng bao gồm các hàm tiện ích để lấy địa chỉ IP
-// của thiết bị và host, đồng thời đảm bảo
-// chỉ tạo một instance API duy nhất(singleton pattern).
