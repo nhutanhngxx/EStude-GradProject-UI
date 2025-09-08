82c9f1f21e914a89d831f355e62ad38230950c57
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-09-08 20:03:33 +0700

    feat: Update AssignmentListModal layout with improved positioning and sizing

--- EStude-Web-UI/src/pages/teacher/AssignmentListModal.jsx
+++ EStude-Web-UI/src/pages/teacher/AssignmentListModal.jsx
@@ -100,8 +100,8 @@ export default function AssignmentListModal({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/30 flex justify-center items-center z-50">
-      <div className="bg-white rounded-xl w-11/12 max-w-6xl h-4/6 p-6 relative shadow-lg overflow-y-auto">
+    <div className="fixed -top-6 left-0 w-screen h-screen bg-black/40 backdrop-blur-sm flex justify-center items-center">
+      <div className="bg-white rounded-xl w-2/5 max-w-6xl h-3/5 p-6 relative shadow-lg overflow-y-auto">
         {/* Nút đóng */}
         <button
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
