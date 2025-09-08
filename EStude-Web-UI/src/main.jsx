9da891809fa504cff7a190def80a3dca8c786cf0
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-09-08 20:05:17 +0700

    feat: Wrap App component with ToastProvider for toast notifications management

--- EStude-Web-UI/src/main.jsx
+++ EStude-Web-UI/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { ToastProvider } from "./contexts/ToastContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </AuthProvider>
   </StrictMode>
 );
