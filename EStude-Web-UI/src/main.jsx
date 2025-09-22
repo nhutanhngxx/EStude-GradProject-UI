1066d5be81c8815e01266dea6d91733572a64599
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-09-22 23:12:41 +0700

    feat: Update CSS variables for improved theming and refactor component hierarchy in main.jsx

--- EStude-Web-UI/src/main.jsx
+++ EStude-Web-UI/src/main.jsx
@@ -6,15 +6,18 @@ import { AuthProvider } from "./contexts/AuthContext.jsx";
 import { ToastProvider } from "./contexts/ToastContext.jsx";
 import { ConfirmProvider } from "./contexts/ConfirmContext";
 import "./i18n";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <ConfirmProvider>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      </ConfirmProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <ConfirmProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </ConfirmProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>
 );
