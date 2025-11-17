3c3fc1c07128fbaad8b4c1dc0ae9d418834bf505
DinhNguyenChung <dinhvanchung24042003@gmail.com>
2025-11-17 23:30:42 +0700

    fix: Update BASE_URL configuration for production deployment

--- EStude-Web-UI/src/config/config.js
+++ EStude-Web-UI/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-  BASE_URL: "http://localhost:8080",
-  // BASE_URL: "https://estude-backend-spring.fly.dev",
+  // BASE_URL: "http://localhost:8080",
+  BASE_URL: "https://estude-backend-spring.fly.dev",
 };
 
 export default config;
