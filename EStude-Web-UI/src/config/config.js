1c10d3b7939831c805f26d7069787f62f587c71c
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-12-12 13:30:55 +0700

    Update BASE_URL configuration: Uncomment production URL and comment out local development URL for deployment.

--- EStude-Web-UI/src/config/config.js
+++ EStude-Web-UI/src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
-  BASE_URL: "http://localhost:8080",
-  // BASE_URL: "https://estude-backend-spring.fly.dev",
+  // BASE_URL: "http://localhost:8080",
+  BASE_URL: "https://estude-backend-spring.fly.dev",
+  // BASE_URL: "https://estude-backend-spring.fly.dev/api/ai/health",
 };
 
 export default config;
